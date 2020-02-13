42bb89fe217f37c74923db8e6e980239386dffd6
isane <isanejossy@gmail.com>
2020-02-13 10:56:39 +0200

    Updates

---
+++ precache-manifest.255bdf7dabbbba2f464c6fa942f105a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d524b9994f9c513b47cbb85a907b9a97",
+    "url": "/MossAndSonHoldings/index.html"
+  },
+  {
+    "revision": "ff2cf7a212ebadc42ab7",
+    "url": "/MossAndSonHoldings/static/css/main.a74463b0.chunk.css"
+  },
+  {
+    "revision": "5b91b17011386a3366ca",
+    "url": "/MossAndSonHoldings/static/js/2.10b10b2b.chunk.js"
+  },
+  {
+    "revision": "ff2cf7a212ebadc42ab7",
+    "url": "/MossAndSonHoldings/static/js/main.7666e23f.chunk.js"
+  },
+  {
+    "revision": "210b54d88416144ae648",
+    "url": "/MossAndSonHoldings/static/js/runtime~main.e0d80483.js"
+  },
+  {
+    "revision": "dac012b27d2f94c849c61bcbe3b69e1c",
+    "url": "/MossAndSonHoldings/static/media/construction.dac012b2.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/MossAndSonHoldings/static/media/map-image.f40ed5b1.png"
+  }
+]);
